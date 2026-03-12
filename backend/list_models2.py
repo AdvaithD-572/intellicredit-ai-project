4ffb3ecf3cc1e7674fba8192e0c6fc2efbf683ea
AdvaithD-572 <dabilipuramadvaith@gmail.com>
2026-03-12 20:48:18 +0530

    Update list_models2.py

--- backend/list_models2.py
+++ backend/list_models2.py
@@ -1,8 +1,11 @@
 import google.generativeai as genai
 import sys
+import streamlit as st
+
+GEMINI_API_KEY = st.secrets["API_KEY"]
 
 try:
-    genai.configure(api_key='AIzaSyBzfxAT7VXoMDpdduYnZfYzcG-jjK9shKM')
+    genai.configure(api_key='GEMINI_API_KEY')
     models = [m.name for m in genai.list_models() if 'generateContent' in m.supported_generation_methods]
     with open("models.txt", "w") as f:
         f.write("\n".join(models))
